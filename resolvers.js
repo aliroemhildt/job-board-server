2285b93fc6c22de6ec38abc350fd745404abe68f
Ali Roemhildt <ali.roemhildt@slalom.com>
2022-09-07 12:02:05 -0600

    Update shema and resolvers with to include company

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,13 @@
+import { Job, Company } from './db.js';
+
 export const resolvers = {
   Query: {
-    jobs: () => [], 
+    jobs: () => Job.findAll()
+  },
+
+  Job: {
+    company: (job) => {
+      return Company.findById(job.companyId);
+    }
   }
 }
